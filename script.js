b8fed5069f50f2cad905c86111b1666ddfac3352
kirtick28 <kirtick.mm2023cse@sece.ac.in>
2025-01-10 10:33:10 +0530

    Changes

---
+++ script.js
@@ -0,0 +1,49 @@
+let display = '';
+
+function inputValue(value) {
+  const displayElement = document.querySelector("input[type='text']");
+  display += value;
+  displayElement.value = display;
+}
+
+function calculate() {
+  try {
+    const displayElement = document.querySelector("input[type='text']");
+    display = eval(display).toString();
+    displayElement.value = display;
+  } catch (error) {
+    alert('Invalid expression!');
+  }
+}
+
+function clearResult() {
+  display = '';
+  document.querySelector("input[type='text']").value = '';
+}
+
+function deleteLast() {
+  const displayElement = document.querySelector("input[type='text']");
+  display = display.slice(0, -1);
+  displayElement.value = display;
+}
+
+const body = document.querySelector('body');
+const button = document.querySelectorAll('button');
+
+const color = [
+  'red',
+  'black',
+  'blue',
+  'pink',
+  'yellow',
+  'green',
+  'violet',
+  'whitesmoke'
+];
+
+button.forEach((button) =>
+  button.addEventListener('click', () => {
+    const colorIndex = parseInt(Math.random() * color.length);
+    body.style.backgroundColor = color[colorIndex];
+  })
+);
